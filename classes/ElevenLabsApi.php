0d6d3d8d1bb8d09df852e031b054f320afcb4b47
kp <kp@redacted.gov>
2023-03-16 14:00:25 -0700

    Rewrite Eleven Labs API class with better commenting and strict type hinting

--- classes/ElevenLabsApi.php
+++ classes/ElevenLabsApi.php
@@ -1,72 +1,124 @@
 <?php
 
+declare(strict_types=1);
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Response;
+
 class ElevenLabsApi
 {
-    const BASE_URL = 'https://api.elevenlabs.io';
+    private const BASE_URL = 'https://api.elevenlabs.io';
 
-    private $apiKey;
+    private string $apiKey;
+    private Client $client;
 
-    public function __construct($apiKey)
+    /**
+     * ElevenLabsApi constructor.
+     *
+     * @param string $apiKey
+     * @param Client|null $client
+     */
+    public function __construct(string $apiKey, Client $client = null)
     {
         $this->apiKey = $apiKey;
+        $this->client = $client ?? new Client();
     }
 
-    private function makeRequest($method, $url, $params = [], $headers = [])
-    {
-        // Implement the logic for making requests using cURL or another HTTP client library
-    }
-
-    public function getHistory()
+    /**
+     * @return Response
+     */
+    public function getHistory(): Response
     {
         $url = self::BASE_URL . '/v1/history';
-        $headers = ['xi-api-key' => $this->apiKey];
-        return $this->makeRequest('GET', $url, [], $headers);
+        return $this->makeRequest('GET', $url);
     }
 
-    public function getAudioFromHistoryItem($historyItemId)
-    {
-        $url = self::BASE_URL . "/v1/history/{$historyItemId}/audio";
-        $headers = ['xi-api-key' => $this->apiKey];
-        return $this->makeRequest('GET', $url, [], $headers);
-    }
-
-    public function deleteHistoryItems(array $historyItemIds)
-    {
-        $url = self::BASE_URL . '/v1/history/delete';
-        $headers = ['xi-api-key' => $this->apiKey];
-        $body = json_encode(['history_item_ids' => $historyItemIds]);
-        return $this->makeRequest('POST', $url, $body, $headers);
-    }
-
-    public function deleteHistoryItem($historyItemId)
+    /**
+     * @param string $historyItemId
+     * @return Response
+     */
+    public function getAudioFromHistoryItem(string $historyItemId): Response
     {
         $url = self::BASE_URL . "/v1/history/{$historyItemId}";
-        $headers = ['xi-api-key' => $this->apiKey];
-        return $this->makeRequest('DELETE', $url, [], $headers);
+        return $this->makeRequest('GET', $url);
     }
 
-    public function downloadHistoryItems(array $historyItemIds)
+    /**
+     * @param array $historyItemIds
+     * @return Response
+     */
+    public function deleteHistoryItems(array $historyItemIds): Response
+    {
+        $url = self::BASE_URL . '/v1/history/delete';
+        $body = json_encode(['history_item_ids' => $historyItemIds]);
+        return $this->makeRequest('POST', $url, $body);
+    }
+
+    /**
+     * @param string $historyItemId
+     * @return Response
+     */
+    public function deleteHistoryItem(string $historyItemId): Response
+    {
+        $url = self::BASE_URL . "/v1/history/{$historyItemId}";
+        return $this->makeRequest('DELETE', $url);
+    }
+
+    /**
+     * @param array $historyItemIds
+     * @return Response
+     */
+    public function downloadHistoryItems(array $historyItemIds): Response
     {
         $url = self::BASE_URL . '/v1/history/download';
-        $headers = ['xi-api-key' => $this->apiKey];
         $body = json_encode(['history_item_ids' => $historyItemIds]);
-        return $this->makeRequest('POST', $url, $body, $headers);
+        return $this->makeRequest('POST', $url, $body);
     }
 
-    public function textToSpeech($voiceId, array $data)
+    /**
+     * @param string $voiceId
+     * @param array $data
+     * @return Response
+     */
+    public function textToSpeech(string $voiceId, array $data): Response
     {
         $url = self::BASE_URL . "/v1/text-to-speech/{$voiceId}/stream";
-        $headers = ['xi-api-key' => $this->apiKey];
         $body = json_encode($data);
-        return $this->makeRequest('POST', $url, $body, $headers);
+        return $this->makeRequest('POST', $url, $body);
     }
-
-    public function textToSpeechWithVoiceId($voiceId, array $data)
+    /**
+     * @param string $voiceId
+     * @param array $data
+     * @return Response
+     */
+    public function textToSpeechWithVoiceId(string $voiceId, array $data): Response
     {
         $url = self::BASE_URL . "/v1/text-to-speech/{$voiceId}";
-        $headers = ['xi-api-key' => $this->apiKey];
         $body = json_encode($data);
-        return $this->makeRequest('POST', $url, $body, $headers);
+        return $this->makeRequest('POST', $url, $body);
     }
 
+    /**
+     * Make a request to the ElevenLabs API.
+     *
+     * @param string $method
+     * @param string $url
+     * @param string|null $body
+     * @return Response
+     */
+    private function makeRequest(string $method, string $url, string $body = null): Response
+    {
+        $options = [
+            'headers' => [
+                'Xi-Api-Key' => $this->apiKey,
+                'Content-Type' => 'application/json',
+            ],
+        ];
+
+        if ($body !== null) {
+            $options['body'] = $body;
+        }
+
+        return $this->client->request($method, $url, $options);
+    }
 }
