0998d860c4207e260b9288c66bd85c539b979e3e
kp <kp@redacted.gov>
2023-03-16 14:57:23 -0700

    Add AppConfig class that loads from config/config.json to retrieve the API keys
    Signed-off-by: kp <kp@redacted.gov>

---
+++ classes/AppConfig.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Classes;
+
+use Psr\Log\LoggerInterface;
+use SplFileObject;
+
+class AppConfig
+{
+    private LoggerInterface $logger;
+    private array $config;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+        $this->loadConfig();
+    }
+
+    private function loadConfig(): void
+    {
+        try {
+            $configFile = new SplFileObject(__DIR__ . '/../config/config.json', 'r');
+            $configContents = $configFile->fread($configFile->getSize());
+            $this->config = json_decode($configContents, true);
+        } catch (\Exception $exception) {
+            $this->logger->error('Failed to load the configuration file.', [
+                'exception' => $exception
+            ]);
+            throw $exception;
+        }
+    }
+
+    public function getApiKey(string $className): string
+    {
+        if (!isset($this->config[$className])) {
+            $errorMessage = "API key not found for class {$className}.";
+            $this->logger->error($errorMessage);
+            throw new \InvalidArgumentException($errorMessage);
+        }
+
+        return $this->config[$className];
+    }
+}
