9331a9a77ce68585c6bf98abccd0abf57aa4be3a
kp <kp@redacted.gov>
2023-03-16 21:01:58 -0700

    Update scene script with more logging and multiple image generation based on frames per image.

--- scene.php
+++ scene.php
@@ -23,76 +23,87 @@ $openai = new OpenAI($openaiApiKey, null, $log);
 $elevenLabsApi = new ElevenLabsApi($elevenLabsApiKey, null, $log);
 
 $log_data = [];
-$prompt = "Low-key good version of harry potter";
+$prompt = 'Something short and gentle that will blow everyones socks off about neuralink';
 // Generate script if it does not exist
 if (!file_exists(__DIR__ . '/scripts/' . md5($prompt) . '.txt')) {
-    // Generate script
-    $role = "you are a scriptwriter from William S Burroughs era. respond as he would.";
-    $script = $openai->generateScript($role, $prompt);
-    file_put_contents(__DIR__ . '/scripts/' . md5($prompt) . '.txt', $script);
+	// Generate script
+	$role = 'you are a scriptwriter from William S Burroughs era. respond as he would.';
+	$script = $openai->generateScript($role, $prompt);
+	file_put_contents(__DIR__ . '/scripts/' . md5($prompt) . '.txt', $script);
 } else {
-    $log_data['txtprompt_search'] = true;
-    $script = file_get_contents(__DIR__ . '/scripts/' . md5($prompt) . '.txt');
+	$log_data['txtprompt_search'] = true;
+	$script = file_get_contents(__DIR__ . '/scripts/' . md5($prompt) . '.txt');
 }
 
 // Generate image prompt if it does not exist
 if (!file_exists(__DIR__ . '/image_prompts/' . md5($prompt) . '.txt')) {
-    // Generate image prompt
-    $role = 'you are a brilliant AI prompt writer. create an image prompt based on this script.';
-    $image_prompt = $openai->generateScript($role, $script);
-    file_put_contents(__DIR__ . '/image_prompts/' . md5($prompt) . '.txt', $image_prompt);
+	// Generate image prompt
+	$role = 'you are a brilliant AI prompt writer. create an image prompt based on this script.';
+	$image_prompt = $openai->generateScript($role, $script);
+	file_put_contents(__DIR__ . '/image_prompts/' . md5($prompt) . '.txt', $image_prompt);
 } else {
-    $image_prompt = file_get_contents(__DIR__ . '/image_prompts/' . md5($prompt) . '.txt');
-    $log_data['imgprompt_search'] = true;
+	$image_prompt = file_get_contents(__DIR__ . '/image_prompts/' . md5($prompt) . '.txt');
+	$log_data['imgprompt_search'] = true;
 }
 
-// Generate images if they do not exist
-if (!file_exists(__DIR__ . '/images/' . md5($prompt))) {
-    mkdir(__DIR__ . '/images/' . md5($prompt));
-    $images = $openai->generateImage($image_prompt, 'images/' . md5($prompt));
-    $log_data['images'] = $images;
-} else {
-    $images = [];
-    $imagesPath = __DIR__ . '/images/' . md5($prompt);
-    $log->info('Checking imagesPath '.$imagesPath);
-    foreach (glob($imagesPath . '/*.png') as $image) {
-        $images[] = $image;
-    }
-    $log_data['image_search'] = true;
-}
 $audio_file = __DIR__ . '/voices/' . md5($prompt) . '.mp3';
 if (!file_exists($audio_file)) {
-    // Generate audio
-    $audio_data = [
-        'text' => $script,
-        'voiceId' => '7ZkBWSrJynvq6BBQZOnf'
-    ];
-    $audio_response = $elevenLabsApi->textToSpeechWithVoiceId($audio_data['voiceId'], $audio_data);
-    file_put_contents($audio_file, $audio_response->getBody());
+	// Generate audio
+	$audio_data = [
+		'text' => $script,
+		'voiceId' => '7ZkBWSrJynvq6BBQZOnf'
+	];
+	$audio_response = $elevenLabsApi->textToSpeechWithVoiceId($audio_data['voiceId'], $audio_data);
+	file_put_contents($audio_file, $audio_response->getBody());
 } else {
-    $log_data['audio_cache'] = true;
+	$log_data['audio_cache'] = true;
 }
 
 try {
+	// Calculate the duration of the audio file
+	$getID3 = new getID3;
+	$file_info = $getID3->analyze($audio_file);
+	$audio_duration = $file_info['playtime_seconds'];
+    $seconds_per_image = 6;
+    $frames_per_second = 25;
+    $frames_per_image = $seconds_per_image * $frames_per_second;
+    $number_of_images = intval($audio_duration / $seconds_per_image);
+    $log->info('Creating ' . $number_of_images . ' images for a ' . $audio_duration . ' second audio clip!');
+
+	// Generate images if they do not exist
+	if (!file_exists(__DIR__ . '/images/' . md5($prompt))) {
+		mkdir(__DIR__ . '/images/' . md5($prompt));
+		$images = $openai->generateImage($image_prompt, 'images/' . md5($prompt), '1024x1024', $number_of_images);
+		$log_data['images'] = $images;
+	} else {
+		$images = [];
+		$imagesPath = __DIR__ . '/images/' . md5($prompt);
+		$log->info('Checking imagesPath ' . $imagesPath);
+		foreach (glob($imagesPath . '/*.png') as $image) {
+			$images[] = $image;
+		}
+		$log_data['image_search'] = true;
+	}
+
 	// Create MeltProject
 	echo('Begin the melty' . PHP_EOL);
-	$project = new MeltProject($log);
+	$project = new MeltProject($log, 1920, 1080, $frames_per_second);
 
 	// Add images to project
-    $log->info('Images: ', $images);
+	$log->info('Images: ', $images);
 	foreach ($images as $image) {
 		$log->info('Adding image ' . $image);
-		$project->addImage($image, 0, 15);
+		$project->addImage($image, 0, $frames_per_image);
 	}
 
 	// Add audio to project
 	$project->setVoiceover($audio_file);
-    $xml = $project->generateXml();
+	$xml = $project->generateXml();
 	// Save project
 	$xml->save('scene.xml');
-    $log->info('End the melt.');
+	$log->info('End the melt.');
 } catch (Throwable $ex) {
-    $log->error($ex->getMessage(), $ex->getTrace());
+	$log->error($ex->getMessage(), $ex->getTrace());
 }
 // Log data
 $log->info('Data:', $log_data);
