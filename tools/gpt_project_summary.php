41bcd01a7af5eb087533dc08fb0f74a1e1ee1b31
kp <kp@redacted.gov>
2023-03-18 19:36:46 -0700

    Update class signatures with return examples
    Signed-off-by: kp <kp@redacted.gov>

--- tools/gpt_project_summary.php
+++ tools/gpt_project_summary.php
@@ -1,11 +1,12 @@
 <?php
 /**
- * Generate a class summary for GPT-4 with class names, methods, signatures, and return values.
+ * Generate a class summary for GPT-4 with class names, methods, signatures, return values, and example return values.
  * Use it to initialize GPT-4 conversations with fewer tokens.
  */
 
 declare(strict_types=1);
 
+// Register an autoloader for classes
 spl_autoload_register(function ($class) {
 	$classPath = __DIR__ . '/../classes/' . $class . '.php';
 	if (file_exists($classPath)) {
@@ -13,6 +14,7 @@ spl_autoload_register(function ($class) {
 	}
 });
 
+// Initialize DirectoryIterator for the classes directory
 $directory = new DirectoryIterator(__DIR__ . '/../classes');
 $outputFile = __DIR__ . '/../class_signatures.txt';
 file_put_contents($outputFile, ''); // Clear output file
@@ -36,8 +38,22 @@ foreach ($directory as $fileInfo) {
 			$returnType = $method->getReturnType() ? ': ' . $method->getReturnType() : '';
 
 			$classSignatures .= "{$method->getName()}({$paramList}){$returnType}\n";
+
+    // Extract example return values from the method's docblock
+    $docComment = $method->getDocComment();
+    if ($docComment !== false) {
+        preg_match('/@return.*\{(?:[^{}]|(?R))*\}/s', $docComment, $matches);
+        if (!empty($matches)) {
+            $openingBracePosition = strpos($matches[0], '{');
+            if ($openingBracePosition !== false) {
+                $exampleReturnValue = trim(substr($matches[0], $openingBracePosition));
+                $classSignatures .= "  Example Return Value: {$exampleReturnValue}\n";
+            }
+        }
+    }
 		}
 
+		// Append class signatures to the output file
 		file_put_contents($outputFile, $classSignatures . "\n", FILE_APPEND);
 	}
 }
